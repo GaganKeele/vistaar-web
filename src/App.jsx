5e92da8152f553607a75419363270f3d766ba9dd
Annupettist <101118433+Annupettist@users.noreply.github.com>
2024-11-12 14:34:21 +0530

    hii

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import React from 'react';
+import Home from './components/home';
+
+
+function App() {
+
+  return (
+    <div className='Main'>
+      <Home></Home>
+      
+      </div>
+  )
+}
+
+export default App;
---
+++ src/components/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div className='home'>
+        <div className='box1'>
+        <div className='stand1'></div>
+        <div className='stand2'></div>
+        </div>
+    </div>
+  )
+}
+
+export default Home
